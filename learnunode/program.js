dafd8f66b40c3a557830f8016f98590921489081
Bradley Bossard <bradleybossard@gmail.com>
2015-03-05 17:07:13 +0000

    Completed challenge 4

--- learnunode/program.js
+++ learnunode/program.js
@@ -1,5 +1,35 @@
+/*
+ // Challenge 2
 var sum = 0;
 for (var i = 2; i < process.argv.length; i++) {
   sum += parseInt(process.argv[i]);
 }
 console.log(sum);
+*/
+
+/*
+// Challenge 3
+var fs = require('fs');
+
+var filename = process.argv[2];
+var buf = fs.readFileSync(filename);
+var numLines = buf.toString().split('\n').length - 1;
+
+console.log(numLines);
+*/
+
+// Challenge 4
+var fs = require('fs');
+
+var filename = process.argv[2];
+var buf = fs.readFile(filename, function(err, data) {
+  if (err) {
+    throw err;
+  }
+
+  var numLines = data.toString().split('\n').length - 1;
+  console.log(numLines);
+
+});
+
+
