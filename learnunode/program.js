590fb85b92a19c51a43be719b84049e6bcd5cd79
Bradley Bossard <bradleybossard@gmail.com>
2015-03-10 18:05:05 +0000

    Added solution for challenge 9

--- learnunode/program.js
+++ learnunode/program.js
@@ -80,11 +80,11 @@ http.get(process.argv[2], function(response) {
 });
 */
 
+/*
 // Challenge 8
 
 var http = require('http');
 var bl = require('bl');
-
 
 http.get(process.argv[2], function(response) {
   response.pipe(bl(function(err, data) {
@@ -97,4 +97,35 @@ http.get(process.argv[2], function(response) {
     console.log(data);
   }));
 });
+*/
+
+// Challenge 9 
+
+var http = require('http')
+var bl = require('bl')
+var results = []
+var count = 0
+
+function printResults () {
+    for (var i = 0; i < 3; i++)
+          console.log(results[i])
+}
+
+function httpGet (index) {
+    http.get(process.argv[2 + index], function (response) {
+          response.pipe(bl(function (err, data) {
+                  if (err)
+                    return console.error(err)
+
+                  results[index] = data.toString()
+                  count++
+
+                  if (count == 3)
+                    printResults()
+                }))
+            })
+}
+
+for (var i = 0; i < 3; i++)
+  httpGet(i)
 
