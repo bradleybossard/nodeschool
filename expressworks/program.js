06ad39f63dc9196dcd331f671dc6a9d2c96c5dd2
Bradley Bossard <bradleybossard@gmail.com>
2015-03-11 18:45:26 +0000

    Complete challenge 3 - Jade

--- expressworks/program.js
+++ expressworks/program.js
@@ -11,11 +11,30 @@ app.get('/home', function(req, res) {
 app.listen(process.argv[2])
 */
 
+/*
 // Challenge 2 - Static
 
 var express = require('express')
 var app = express()
 
 app.use(express.static(process.argv[3]||path.join(__dirname, 'public')));
+
+app.listen(process.argv[2])
+*/
+
+// Challenge 3 - Jade 
+
+var path = require('path')
+var express = require('express')
+var app = express()
+
+app.set('view engine', 'jade')
+app.set('views', path.join(__dirname, 'templates')) 
+
+//app.use(express.static(process.argv[3]||path.join(__dirname, 'public')));
+app.get('/home', function(req, res) {
+  res.render('index', {date: new Date().toDateString()})
+})
+
 
 app.listen(process.argv[2])
