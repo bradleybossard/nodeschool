c3ec30c570812064885db70e0f8e0e5738dd835d
Bradley Bossard <bradleybossard@gmail.com>
2015-03-11 18:35:34 +0000

    Added solution for Challege 2 - Static

--- expressworks/program.js
+++ expressworks/program.js
@@ -1,8 +1,21 @@
 // Challenge 1 - Hello World
+/*
+var express = require('express')
+var app = express()
+
+app.get('/home', function(req, res) {
+  res.end('Hello World!')
+
+})
+
+app.listen(process.argv[2])
+*/
+
+// Challenge 2 - Static
 
 var express = require('express')
 var app = express()
-app.get('/home', function(req, res) {
-  res.end('Hello World!')
-})
+
+app.use(express.static(process.argv[3]||path.join(__dirname, 'public')));
+
 app.listen(process.argv[2])
